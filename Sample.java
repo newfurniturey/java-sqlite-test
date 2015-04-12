3cd611d615096cd9ce3aa54bba3d72e826b45463
Joey <joey@tumblr.com>
2015-04-12 02:45:07 -0400

    More refactor

--- Sample.java
+++ Sample.java
@@ -1,28 +1,35 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Sample {
     private static String DB_NAME = "sample.db";
+    private static Connection _connection = null;
     
-    public Connection connect() throws SQLException {
-        return DriverManager.getConnection("jdbc:sqlite:" + DB_NAME);
+    public void connect() throws SQLException {
+        _connection = DriverManager.getConnection("jdbc:sqlite:" + DB_NAME);
     }
     
-    public void createTable(Statement stmt) throws SQLException {
-        stmt.executeUpdate("DROP TABLE IF EXISTS person;");
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS person (id integer, name string);");
+    public void createTable() throws SQLException {
+        Statement statement = _connection.createStatement();
+        statement.executeUpdate("DROP TABLE IF EXISTS person;");
+        statement.executeUpdate("CREATE TABLE IF NOT EXISTS person (id integer, name string);");
+        addSampleData();
     }
     
-    public void addSampleData(Statement stmt) throws SQLException {
-        stmt.executeUpdate("insert into person values(1, 'leo');");
-        stmt.executeUpdate("insert into person values(2, 'yui');");
+    public void addSampleData() throws SQLException {
+        Statement statement = _connection.createStatement();
+        statement.executeUpdate("INSERT INTO person VALUES (1, 'leo');");
+        statement.executeUpdate("INSERT INTO person VALUES (2, 'geordie');");
+        statement.executeUpdate("INSERT INTO person VALUES (3, 'yui');");
     }
     
-    public ResultSet query(Statement stmt) throws SQLException {
-        return stmt.executeQuery("SELECT id, name FROM person;");
+    public ResultSet query() throws SQLException {
+        Statement statement = _connection.createStatement();
+        return statement.executeQuery("SELECT id, name FROM person;");
     }
     
     public static void main(String[] args) throws ClassNotFoundException {
@@ -30,17 +37,12 @@ public class Sample {
         Class.forName("org.sqlite.JDBC");
 
         Sample testDb = new Sample();
-        Connection connection = null;
         try {
             // create a database connection
-            connection = testDb.connect();
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);  // set timeout to 30 sec.
-
-            testDb.createTable(statement);
-            testDb.addSampleData(statement);
+            testDb.connect();
+            testDb.createTable();
             
-            ResultSet rs = testDb.query(statement);
+            ResultSet rs = testDb.query();
             if (!rs.isBeforeFirst()) {
                 System.out.println("no data found.");
             } else {
@@ -59,8 +61,8 @@ public class Sample {
             System.err.println(e.getMessage());
         } finally {
             try {
-                if (connection != null) {
-                    connection.close();
+                if (_connection != null) {
+                    _connection.close();
                 }
             } catch (SQLException e) {
                 // connection close failed.
